f94015e7cd953927a2c1be17026254377785e44b
Ravi Singla <rkravi.98@gmail.com>
2018-08-09 19:45:30 +0530

    rocket funcationality added

---
+++ Assets/rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        processInput();
+	}
+
+    private void processInput()
+    {
+       if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+    }
+}
